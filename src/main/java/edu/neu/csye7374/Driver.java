19de626c73fec6323748c8aa7c0c4c5b19a72b92
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2025-05-20 21:38:12 -0400

    Stock API UPdated Driver

--- src/main/java/edu/neu/csye7374/Driver.java
+++ src/main/java/edu/neu/csye7374/Driver.java
@@ -18,42 +18,7 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("============Main Execution Start===================\n\n");
 
-		StockMarket stockMarket = StockMarket.getInstance();
-
-		Stock jpm = new FinTechStock("JPM", 1000.0, "JP Morgan Chase");
-		Stock wal = new ECommStock("WAL", 300.0, "Walmart");
-		Stock siemens = new HealthcareStock("SIH", 100.0, "Siemens Healthineers");
-		Stock msft = new TechStock("MSFT", 2000.0, "Microsoft Corporation");
-		Stock edn = new EducationStock("EDN", 500.0, "Edu Nova");
-
-		stockMarket.addStock(jpm);
-		stockMarket.addStock(wal);
-		stockMarket.addStock(siemens);
-		stockMarket.addStock(msft);
-		stockMarket.addStock(edn);
-
-		for (double bid : new double[]{1510, 1490, 1520, 1505, 1515, 1495}) {
-			stockMarket.tradeStock(jpm, String.valueOf(bid));
-		}
-		
-		for (double bid : new double[]{490, 510, 520, 480, 495, 505}) {
-			stockMarket.tradeStock(edn, String.valueOf(bid));
-		}
-
-		for (double bid : new double[]{310, 305, 300, 295, 290, 285}) {
-			stockMarket.tradeStock(wal, String.valueOf(bid));
-		}
-
-		for (double bid : new double[]{110, 101, 104, 109, 99, 103}) {
-			stockMarket.tradeStock(siemens, String.valueOf(bid));
-		}
-
-		// Tech stock with more volatile price movements to demonstrate the metric
-		for (double bid : new double[]{2100, 1950, 2200, 1900, 2300, 1850}) {
-			stockMarket.tradeStock(msft, String.valueOf(bid));
-		}
-
-		stockMarket.showAllStocks();
+		StockMarket.demo();
 		 
 		System.out.println("\n\n============Main Execution End===================");
 	}
--- src/main/java/edu/neu/csye7374/factory/StockMarket.java
+++ src/main/java/edu/neu/csye7374/factory/StockMarket.java
@@ -2,7 +2,7 @@ package edu.neu.csye7374.factory;
 
 
 
-import edu.neu.csye7374.factory.stock.Stock;
+import edu.neu.csye7374.factory.stock.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,5 +36,54 @@ public class StockMarket {
 
     public void showAllStocks() {
         stocks.forEach(System.out::println);
+    }
+
+    public static void demo() {
+
+        StockMarket stockMarket = StockMarket.getInstance();
+
+        Stock jpm = new FinTechStock("JPM", 1000.0, "JP Morgan Chase");
+        Stock wal = new ECommStock("WAL", 300.0, "Walmart");
+        Stock siemens = new HealthcareStock("SIH", 100.0, "Siemens Healthineers");
+        Stock msft = new TechStock("MSFT", 2000.0, "Microsoft Corporation");
+        Stock edn = new EducationStock("EDN", 500.0, "Edu Nova");
+
+        stockMarket.addStock(jpm);
+        stockMarket.addStock(wal);
+        stockMarket.addStock(siemens);
+        stockMarket.addStock(msft);
+        stockMarket.addStock(edn);
+
+        for (double bid : new double[]{1510, 1490, 1520, 1505, 1515, 1495}) {
+            stockMarket.tradeStock(jpm, String.valueOf(bid));
+        }
+
+        for (double bid : new double[]{490, 510, 520, 480, 495, 505}) {
+            stockMarket.tradeStock(edn, String.valueOf(bid));
+        }
+
+        for (double bid : new double[]{310, 305, 300, 295, 290, 285}) {
+            stockMarket.tradeStock(wal, String.valueOf(bid));
+        }
+
+        for (double bid : new double[]{110, 101, 104, 109, 99, 103}) {
+            stockMarket.tradeStock(siemens, String.valueOf(bid));
+        }
+
+        // Tech stock with more volatile price movements to demonstrate the metric
+        for (double bid : new double[]{2100, 1950, 2200, 1900, 2300, 1850}) {
+            stockMarket.tradeStock(msft, String.valueOf(bid));
+        }
+
+        System.out.println("\n\n============All Stocks===================");
+        stockMarket.showAllStocks();
+
+        System.out.println("\n\n============Removing a Stock===================");
+        System.out.println("Removing stock: " + jpm);
+        stockMarket.removeStock(jpm);
+
+        System.out.println("\n\n============All Stocks===================");
+        stockMarket.showAllStocks();
+
     }
 }
